f44ce127a9037f5340dafa9f53943ae2938c8f78
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-11-18 23:31:06 +0000

    Fix typo

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,7 +5,7 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        excludes = ('user',)
+        exclude = ('user',)
 
     def __init__(self, *args, **kwargs):
         """
