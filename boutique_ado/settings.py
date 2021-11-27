c5af8f36283648af19ea07a6ab638eafbdead9ce
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-11-27 14:44:45 +0000

    Add allowed hosts and if/else for databases

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*1#3i5-$f3ec2l7m4fg=rz5lb-5r$w(mmeet=v3yo=!5p%!a6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-nat.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
